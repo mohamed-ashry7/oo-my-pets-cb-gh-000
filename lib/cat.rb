da38430dae170217202275ec7526e3e69f918cd0
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 18:11:36 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -18,7 +18,7 @@ class Cat
   end
 
 
-  def find_or_create_a_cat(name, owner )
+  def self.find_or_create_a_cat(name, owner )
     cat = @@all.find {|c| c.name == name  }
     cat.owner = owner if cat
     cat ||=self.new(name,owner )
