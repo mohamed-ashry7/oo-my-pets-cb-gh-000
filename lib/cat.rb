db46fbaa7b770203e7509d54b10645c7bf03b4c8
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 17:57:29 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,16 @@
 class Cat
-  # code goes here
+  attr_accessor :owner ,  :mood
+  attr_reader :name
+  @@all =[] 
+  def initialize(name , owner )
+    @name = name
+    @owner = owner
+  end
+
+  def self.all
+    @@all
+  end
+  def self.count
+    @@all.size
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,26 @@
 class Owner
-  # code goes here
+  attr_accessor :pets
+  attr_reader :name , :species
+  @@all = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @pets = []
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{species}"
+  end
+
+  def self.all
+    @@all
+  end
+  def self.count
+    @@all.size
+  end
+  def reset_all
+    @all.clear
+  end
+
 end
