c1392e857235958901c8d0eca5bbb5f49fe9b446
Mohamed Ashry <mohamedelashry7777@gmail.com>
2020-02-13 18:24:48 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,7 +2,7 @@ class Cat
   attr_accessor :owner ,  :mood
   attr_reader :name
   @@all =[]
-  def initialize(name , ownerl )
+  def initialize(name , owner )
     @name = name
     @owner = owner
     @mood = "nervous"
@@ -19,7 +19,7 @@ class Cat
 
   def self.find_or_create_a_cat(name , owner )
     cat = @@all.find {|c| c.name == name  }
-    cat.owner = owner if cat 
+    cat.owner = owner if cat
     cat ||=self.new(name,owner)
   end
 end
